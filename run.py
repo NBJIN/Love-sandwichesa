a431847bf6596ddb94f3a229f14f55a735b83851
noreen <noreennelligan1@gmail.com>
2022-10-22 07:50:00 +0000

    changemade not correct

--- run.py
+++ run.py
@@ -1,20 +1,21 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from pprint import pprint
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint 
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
+    "https://www.googleapis.com/auth/drive",
     ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
 
 def get_sales_data():
     """
@@ -27,35 +28,33 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
-    
         sales_data = data_str.split(",")
-                  
-
         if validate_data(sales_data):
             print("Data is valid!")
             break
 
     return sales_data
 
+
 def validate_data(values):
     """
-    Inside the try, converts all string values into integers.
-    Raises ValueError if strings cannot be converted into int,
-    or if there aren't exactly 6 values. 
+        Inside the try, converts all string values into integers.
+        Raises ValueError if strings cannot be converted into int,
+        or if there aren't exactly 6 values.  
     """
-
     print(values)
     try:
         [int(value) for value in values]
-        if len(values) !=6:
+        if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-        return False 
+    return False
 
     return True
+
 
 def update_sales_worksheet(data):
     """
@@ -66,17 +65,23 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+
 def calculate_surplus_data(sales_row):
-    """
-    Compare sales with stock and calculate the surplus for each item type.
-    The surplus is defined as the sales figure subtracted from the stock:
-    -Positive suprlus indicates waste
-    - Negative surplus indicates extra made when stock was sold out. 
-    """
+     """
+     Compare sales with stock and calculate the surplus for each item type.
+        The surplus is defined as the sales figure subtracted from the stock:
+        -Positive suprlus indicates waste
+        -Negative surplus indicates extra made when stock was sold out. 
+     """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    print(surplus_data)
 
 
 def main():
@@ -88,6 +93,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     calculate_surplus_data(sales_data)
+
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
